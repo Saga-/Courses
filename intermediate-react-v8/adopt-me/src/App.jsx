5fc1ff14238dad72b7893a917127c1229cf48761
Saga- <saga@saga.com>
2024-01-02 14:57:40 +0000

    low-priority rerendering

--- intermediate-react-v8/adopt-me/src/App.jsx
+++ intermediate-react-v8/adopt-me/src/App.jsx
@@ -1,17 +1,16 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useState, lazy, Suspense } from "react";
+import { useState } from "react";
 import AdoptedPetContext from "./AdoptedPetContext";
-
-const Details = lazy(() => import("./Details"));
-const SearchParams = lazy(() => import("./SearchParams"));
+import Details from "./Details";
+import SearchParams from "./SearchParams";
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: Infinity,
       cacheTime: Infinity,
-      suspense: true,
     },
   },
 });
@@ -20,26 +19,23 @@ const App = () => {
   const adoptedPet = useState(null);
   return (
     <div>
-      <AdoptedPetContext.Provider value={adoptedPet}>
-        <QueryClientProvider client={queryClient}>
-          <Suspense
-            fallback={
-              <div className="loading-pane">
-                <h2 className="loader">Dog</h2>
-              </div>
-            }
-          ></Suspense>
-          <header>
-            <Link to="/">Adopt Me!</Link>
-          </header>
-          <Routes>
-            <Route path="/details/:id" element={<Details />} />
-            <Route path="/" element={<SearchParams />} />
-          </Routes>
-        </QueryClientProvider>
-      </AdoptedPetContext.Provider>
+      <BrowserRouter>
+        <AdoptedPetContext.Provider value={adoptedPet}>
+          <QueryClientProvider client={queryClient}>
+            <header>
+              <Link to="/">Adopt Me!</Link>
+            </header>
+            <Routes>
+              <Route path="/details/:id" element={<Details />} />
+              <Route path="/" element={<SearchParams />} />
+            </Routes>
+          </QueryClientProvider>
+        </AdoptedPetContext.Provider>
+      </BrowserRouter>
     </div>
   );
 };
 
-export default App;
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
